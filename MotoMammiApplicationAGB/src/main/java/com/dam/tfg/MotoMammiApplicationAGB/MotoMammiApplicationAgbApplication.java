e96de2f82765c6bfa4a9090622240d9d6a2b81bf
SikaFlex <agarcia30@nigul.cide.es>
2024-03-12 16:37:03 +0100

    firts

---
+++ MotoMammiApplicationAGB/src/main/java/com/dam/tfg/MotoMammiApplicationAGB/MotoMammiApplicationAgbApplication.java
@@ -0,0 +1,13 @@
+package com.dam.tfg.MotoMammiApplicationAGB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoMammiApplicationAgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoMammiApplicationAgbApplication.class, args);
+	}
+
+}
