1ba9dbcbfb278641f69b87a5cad6084b4a3c8e5c
SikaFlex <agarcia30@nigul.cide.es>
2024-03-16 01:14:57 +0100

    config

--- MotoMammiApplicationAGB/src/main/java/com/dam/tfg/MotoMammiApplicationAGB/MotoMammiApplicationAgbApplication.java
+++ MotoMammiApplicationAGB/src/main/java/com/dam/tfg/MotoMammiApplicationAGB/MotoMammiApplicationAgbApplication.java
@@ -1,13 +1,57 @@
 package com.dam.tfg.MotoMammiApplicationAGB;
 
-import org.springframework.boot.SpringApplication;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
 
 @SpringBootApplication
 public class MotoMammiApplicationAgbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MotoMammiApplicationAgbApplication.class, args);
+		Session session=null;
+		try{
+			session = getSession();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}finally{
+			session.close();
+		}
+	}
+
+
+	public static Session getSession(){
+		SessionFactory sessionFactory;
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		// configuration.addAnnotatedClass(Covid_Register_User.class);
+		// configuration.addAnnotatedClass(Users_cide.class);
+
+		StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+		return sessionFactory.openSession();
+
 	}
 
 }
