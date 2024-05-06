3928306473027f3d84c4d914c64e3e500e5dfa30
SikaFlex <agarcia30@nigul.cide.es>
2024-05-06 19:39:19 +0200

    sql sentencence update

--- TRABAJO FINAL SQL.sql
+++ TRABAJO FINAL SQL.sql
@@ -1,6 +1,6 @@
 CREATE TABLE MM_PROVIDERS (
-    id VARCHAR(250),
-    codigoProveedor VARCHAR(250),
+    id VARCHAR(250) primary KEY,
+    codigoProveedor VARCHAR(250) ,
     name VARCHAR(250),
     dateIni DATE,
     dateEnd DATE,
@@ -8,6 +8,7 @@ CREATE TABLE MM_PROVIDERS (
     PRIMARY KEY (id)
 );
 
+-- Primera tabla 
 CREATE TABLE MM_INTERFACE (
     id VARCHAR(250),
     idProv varchar(250),
@@ -21,26 +22,82 @@ CREATE TABLE MM_INTERFACE (
     codError VARCHAR(20),
     errorMessage VARCHAR(4000),
     statusProcess VARCHAR(10),
-    operation VARCHAR(250),
+    operation VARCHAR(250),--  New, Update   -- N -> no ha sido procesado  -- E-->  -- P--> Procesado
     resource VARCHAR(20),
-    FOREIGN KEY (idProv) REFERENCES MM_PROVIDERS(id)
+    FOREIGN KEY (codProv) REFERENCES MM_PROVIDERS(id),
+	FOREIGN KEY (codExternal) REFERENCES MM_Customer(DNI)
+
 );
 
+select * from MM_INTERFACE 
+where codExternal = @dni
+and codProv = @CodProv
+and contJson;
+
+--
 CREATE TABLE MM_TRANSLATION (
 id int,
 idProv varchar(250),
 cod_ext varchar(250),
 cod_int varchar(250),
+date_ini date,
+date_end date,
 FOREIGN KEY (idProv) REFERENCES MM_PROVIDERS(id)
-
 );
 
+
+use mm_agb;
+
 -- CREATE TABLE CUSTOMERS
+CREATE TABLE IF NOT EXISTS MM_Customer (
+    DNI VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(100),
+    first_surname VARCHAR(100),
+    last_surname VARCHAR(100),
+    email VARCHAR(100),
+    birth_date DATE,
+    postal_code VARCHAR(20),
+    street_type VARCHAR(50),
+    city VARCHAR(100),
+    number VARCHAR(20),
+    phone VARCHAR(20),
+    gender varchar(20),
+    licence_type VARCHAR(50)
+);
+-- PARTS
+CREATE TABLE MM_PARTS(
+	ID_PROVEEDOR varchar(100) PRIMARY KEY,
+    
+    
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select * from MM_INTERFACE where codExternal = @DNI;
+
 	
 use mm_agb;
 -- Relacion  id de provider -> idProv
 
 Select * from MM_PROVIDERS;
+
 
 -- QUERY COMPROBAR PROVIDERS ACTIVOS
 select * from MM_PROVIDERS 
@@ -48,9 +105,13 @@ where swiact = 1
 and ifnull(@param_date,current_date()) BETWEEN dateIni AND ifnull(dateEnd,'2099-12-31')
 and codigoProveedor = ifnull(@p_prov, codigoProveedor);
 
+
+use mm_agb;
+
 select * from MM_PROVIDERS
-where swiact = 1 and ifnull(null,current_date()) BETWEEN dateIni AND ifnull(null,'2099-12-31') and codigoProveedor = ifnull(@p_prov, codigoProveedor)
+where swiact = 1 and ifnull(null,current_date()) BETWEEN dateIni AND ifnull(null,'2099-12-31') and codigoProveedor = ifnull(@p_prov, codigoProveedor);
+
+
 
 -- and codigoProveedor = @p_Prov and 
 
-[where swiact = 1 and ifnull(:p_date,current_date()) BETWEEN dateIni AND ifnull(dateEnd,'2099-12-31') and codigoProveedor = ifnull(:p_prov, codigoProveedor)]
